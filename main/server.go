9f179ba5c1649156f1e6642c3d4a0fee21b2d97c
OpenSauce <lawleycobhc@gmail.com>
2021-03-15 22:14:07 +0000

    String output test

--- main/server.go
+++ main/server.go
@@ -48,10 +48,14 @@ func executeCommandHandler(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Printf("Recieved the message %s \n", request.Command)
 	output, err := ExecuteCommand(request.Command)
+	// formattedErr := ""
+	// if err != nil {
+	// 	formattedErr = 
+	// }
 
 	fmt.Printf("Got: %s : %s \n", output, err)
 	fmt.Fprintf(w, `{ "output": "%s",
-	"error": "%s" }`, output, err)
+	"error": "%s" }`, output, err.Error())
 }
 
 func enableCors(w *http.ResponseWriter) {
